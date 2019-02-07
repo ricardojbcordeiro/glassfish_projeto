947c140d401fde187067fe566952cd023a0a5321
ricardo <ricardo.jbcordeiro@gmail.com>
2019-02-06 22:11:49 -0300

    adicionando o projeto ejb

---
+++ glassfish_ejb/ejbModule/br/com/glassfish/ejb/TesteEjb.java
@@ -0,0 +1,18 @@
+package br.com.glassfish.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TesteEjb
+ */
+@Stateless
+public class TesteEjb {
+
+    /**
+     * Default constructor. 
+     */
+    public TesteEjb() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
